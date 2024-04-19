4e8624586a5d1923de86ee21c655e57abd963afa
Naim <naeemmas@gmail.com>
2024-04-20 00:15:39 +0300

    remove inline keyword from gpio module

--- GPIO/gpio.h
+++ GPIO/gpio.h
@@ -19,7 +19,6 @@
 #define GPIO_PORT_2 1U
 #define GPIO_PORT_3 2U
 
-
 #define GPIO_SET_IN(port, pin)      BIT_CLR(port, pin)
 #define GPIO_SET_OUT(port,pin)      BIT_SET(port, pin)
 #define GPIO_READ(port, pin)        BIT_CHK(port, pin)
@@ -27,19 +26,19 @@
 #define GPIO_WRITE_LOW(port, pin)   BIT_CLR(port, pin)
 #define GPIO_TOGGLE(port, pin)      BIT_TOG(port, pin)
 
-inline void GPIO_setModeInput(uint8_t port, uint8_t pin);
-inline void GPIO_setModeInputWithPullUp(uint8_t port, uint8_t pin);
-inline void GPIO_setModeInputWithPullDown(uint8_t port, uint8_t pin);
-inline void GPIO_setModeOutput(uint8_t port, uint8_t pin);
-inline void GPIO_setPortOutput(uint8_t port);
-inline void GPIO_setPortInput(uint8_t port); //TODO: test this function
-inline void GPIO_setDigitalPinLow(uint8_t port, uint8_t pin);
-inline void GPIO_setDigitalPinHigh(uint8_t port, uint8_t pin);
-inline void GPIO_writeOuputPort(uint8_t port, uint8_t value);
-inline void GPIO_toggleDigitalPin(uint8_t port, uint8_t pin);
-inline uint8_t GPIO_getDigitalPort(uint8_t port); //TODO: test this function
-inline bool GPIO_getDigitalPin(uint8_t port, uint8_t pin);
-inline void GPIO_enableInterrupt(uint8_t port, uint8_t pin, bool edge_select);
-inline void GPIO_disableInterrupt(uint8_t port, uint8_t pin);
+void GPIO_setModeInput(uint8_t port, uint8_t pin);
+void GPIO_setModeInputWithPullUp(uint8_t port, uint8_t pin);
+void GPIO_setModeInputWithPullDown(uint8_t port, uint8_t pin);
+void GPIO_setModeOutput(uint8_t port, uint8_t pin);
+void GPIO_setPortOutput(uint8_t port);
+void GPIO_setPortInput(uint8_t port); //TODO: test this function
+void GPIO_setDigitalPinLow(uint8_t port, uint8_t pin);
+void GPIO_setDigitalPinHigh(uint8_t port, uint8_t pin);
+void GPIO_writeOuputPort(uint8_t port, uint8_t value);
+void GPIO_toggleDigitalPin(uint8_t port, uint8_t pin);
+uint8_t GPIO_getDigitalPort(uint8_t port); //TODO: test this function
+bool GPIO_getDigitalPin(uint8_t port, uint8_t pin);
+void GPIO_enableInterrupt(uint8_t port, uint8_t pin, bool edge_select);
+void GPIO_disableInterrupt(uint8_t port, uint8_t pin);
 
 #endif /* INC_GPIO_H_ */
